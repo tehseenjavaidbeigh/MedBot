f4c7769ad43fbb9e74139c98a2e1006312f0717d
tehseenjavaidbeigh <tehseenjavaidbeigh@gmail.com>
2026-03-07 10:28:22 +0530

    web app completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name="Medical_chatbot",
+    version ="0.1.0",
+    auther="Tehseen Javaid Beigh",
+    auther_email="tehseenjavaidbeigh@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
